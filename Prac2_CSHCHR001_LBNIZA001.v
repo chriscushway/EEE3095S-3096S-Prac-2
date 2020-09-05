75c5c42eee7445ddab0669a31ffeecab1be5d92e
chriscushway <chriscushway@gmail.com>
2020-09-06 00:35:42 +0200

    define inputs and outputs

--- Prac2_CSHCHR001_LBNIZA001.v
+++ Prac2_CSHCHR001_LBNIZA001.v
@@ -3,3 +3,12 @@
 // Authors     : CSHCHR001, LBNIZA001
 // Function    : Emulate an VS-ALU
 //--------------------------------------------------------
+
+module alu (clk, A, B, opcode, acc, res)
+    input clk;
+    input [3:0] opcode;
+    input [7:0] A,B;
+    output[7:0] res;
+    reg [7:0] acc;
+
+endmodule
