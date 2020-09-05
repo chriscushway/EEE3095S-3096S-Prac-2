cb8c56ca5f1b39a28f72d9b91c826d5caa60f22d
chriscushway <chriscushway@gmail.com>
2020-09-06 01:47:04 +0200

    made adjustments to test bench

--- Prac2_CSHCHR001_LBNIZA001.v
+++ Prac2_CSHCHR001_LBNIZA001.v
@@ -4,26 +4,26 @@
 // Function    : Emulate an VS-ALU
 //--------------------------------------------------------
 
-module ALU(clk,A,B,opcode,acc,ALU_Out);
+module ALU(clk,A,B,opcode,ALU_Out);
     input clk;
     input [3:0] opcode;
     input [7:0] A,B;
-    output reg[7:0] ALU_Out,acc;
-    // reg [7:0] acc;
+    output reg[7:0] ALU_Out;
+    reg [7:0] acc = 8'b00000000; //initialise register to 0
 
     always@(posedge clk) 
         begin
             case(opcode)
                 4'b0000: //ADD
                     acc = A + B;                   
-                // 4'b0001: //SUB
-                //     //do logic here
+                4'b0001: //SUB
+                    acc = A - B;
                 // 4'b0010: //MUL
                 //     //multiply here
                 // 4'b0011: //DIV
                 //     //do logic here
-                // 4'b0100: //ADDA
-                //     //do logic here
+                4'b0100: //ADDA
+                    acc = acc + A;
                 // 4'b0101: //MULA
                 //     //do logic here
                 // 4'b0110: //MAC
--- Prac2_CSHCHR001_LBNIZA001_tb.v
+++ Prac2_CSHCHR001_LBNIZA001_tb.v
@@ -2,33 +2,34 @@ module ALU_tb();
 
     reg clk;
     reg[3:0] opcode;
-    reg [7:0] A,B;
-    wire [7:0] ALU_Out, acc;
+    reg [7:0] A,B, acc;
+    wire [7:0] ALU_Out;
 
     ALU ut(
         .clk(clk),
         .A(A),
         .B(B),
         .opcode(opcode),
-        .acc(acc),
-        .ALU_Out(ALU_Out)    
+        .ALU_Out(ALU_Out)
         );
 
     initial begin 
         $display("clk A B acc opcode ALU_Out");
-        $monitor("%b %b %b %b %b %b", clk, A, B, acc, opcode, ALU_Out);
+        $monitor("%b %b %b %b %b %b", clk, A, B, opcode, ALU_Out);
         $dumpfile("dump.vcd");
         $dumpvars;
-
+        acc = ALU_Out;
         clk = 1'b1; //initially set clock
         A = 8'b00000001;
         B = 8'b00000001;
         #5 clk = !clk;
         #5 clk = !clk;
-        opcode = 4'b0000;
-        repeat(11) begin 
-            #5 clk = !clk;
-        end
-        
+       
+        opcode = 4'b0100;
+        #5 clk = !clk;
+        #5 clk = !clk;
+        #5 clk = !clk;
+        #5 clk = !clk;
+
     end
 endmodule
